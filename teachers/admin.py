4a8400f9e0223c5504e3ac9ddb3ef2b227c673c8
Mike Young <maikelym1990@gmail.com>
2019-11-29 16:06:48 +0100

    fix name error

--- teachers/admin.py
+++ teachers/admin.py
@@ -5,7 +5,7 @@ from teachers.models import Teacher
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'apellidos', 'email', 'telefono')
-    search_fields = ('id', 'nombhre', 'apellidos', 'email')
+    search_fields = ('id', 'nombre', 'apellidos', 'email')
 
 
 admin.site.register(Teacher, TeacherAdmin)
