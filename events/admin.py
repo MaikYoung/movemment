dc6363428bb136de4e9b54565df4128432b04646
Mike Young <maikelym1990@gmail.com>
2019-11-29 16:30:52 +0100

    add event app complete

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('titulo_evento', 'fecha_inicio', 'fecha_fin', 'activo', 'lleno')
+    search_fields = ('titulo_evento', )
+    list_filter = ('activo', 'lleno', 'localizacion_evento')
+
+
+admin.site.register(Event, EventAdmin)
--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'teachers',
     'foods',
     'houses',
+    'events',
 ]
 
 MIDDLEWARE = [
