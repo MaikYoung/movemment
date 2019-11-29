097e5bf9069ccc5922028fd6aa7224fa92c67aed
Mike Young <maikelym1990@gmail.com>
2019-11-29 13:17:44 +0100

    add teacher models

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    apellidos = models.CharField(max_length=100, blank=True, null=True)
+    imagen_url = models.URLField(blank=True, null=True)
+    especialidad_de_yoga = models.TextField(blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    instagram = models.CharField(max_length=100, blank=True, null=True)
+    facebook = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Profesor'
+        verbose_name_plural = 'Profesores'
+
+
